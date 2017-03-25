b3cd482c9fcc82b510e5fc6bbf98869077b02455
paggieli <paggieisme@gmail.com>
2017-03-26 01:30:43 +0800

    create new project

---
+++ CalculateFee.swift
@@ -0,0 +1,35 @@
+//
+//  CalculateFee.swift
+//  CalculateFee
+//
+//  Created by Admin on 2017/3/26.
+//  Copyright © 2017年 Admin. All rights reserved.
+//
+
+import UIKit
+
+class CalculateFee: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
