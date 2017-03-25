6e7ed6e630b49db722c23748d0fd0dc836ac59d3
paggieli <paggieisme@gmail.com>
2017-03-26 02:07:31 +0800

    finish  content

--- CalculateFee.swift
+++ CalculateFee.swift
@@ -9,6 +9,34 @@
 import UIKit
 
 class CalculateFee: UIViewController {
+    
+
+    @IBOutlet weak var txt_currency: UITextField!
+    @IBOutlet weak var txt_rate: UITextField!
+    
+    @IBOutlet weak var label_ans: UILabel!
+    @IBOutlet weak var label_title: UILabel!
+    
+    
+    
+    @IBAction func btn_calc(_ sender: Any) {
+        
+        if(txt_rate.text! != "" && txt_currency.text! != ""){
+          
+            let rate:Double? = Double(txt_rate.text!)
+            let curr:Double? = Double(txt_currency.text!)
+            
+            label_ans.text = "\(( curr! / rate! ) * 30.2)"
+
+        
+            label_ans.isHidden = false
+            label_title.isHidden = false
+        
+        }
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
